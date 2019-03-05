38c7c39d6a94378483ab13c04fb380cae97f3b80
Justin Parado <japarado@student.apc.edu.ph>
2019-03-05 19:19:18 +0800

    Signing up now requires first and last name fields
    This was previously a full name field

--- api/controllers/entrance/signup.js
+++ api/controllers/entrance/signup.js
@@ -35,13 +35,26 @@ the account verification message.)`,
       description: 'The unencrypted password to use for the new account.'
     },
 
-    fullName:  {
+    /*fullName:  {
       required: true,
       type: 'string',
       example: 'Frida Kahlo de Rivera',
       description: 'The user\'s full name.',
-    }
+    },*/
 
+    firstName:  {
+      required: true,
+      type: 'string',
+      example: 'John',
+      description: 'The user\'s first name.',
+    },
+
+    lastName:  {
+      required: true,
+      type: 'string',
+      example: 'Doe',
+      description: 'The user\'s last name.',
+    },
   },
 
 
@@ -75,7 +88,9 @@ the account verification message.)`,
     var newUserRecord = await User.create(_.extend({
       emailAddress: newEmailAddress,
       password: await sails.helpers.passwords.hashPassword(inputs.password),
-      fullName: inputs.fullName,
+      //fullName: inputs.fullName,
+      firstName: inputs.firstName,
+      lastName: inputs.lastName,
       tosAcceptedByIp: this.req.ip
     }, sails.config.custom.verifyEmailAddresses? {
       emailProofToken: await sails.helpers.strings.random('url-friendly'),
--- api/models/User.js
+++ api/models/User.js
@@ -48,12 +48,28 @@ email status until they click the link in the confirmation email.`
       example: '2$28a8eabna301089103-13948134nad'
     },
 
-    fullName: {
+    /*fullName: {
       type: 'string',
       required: true,
       description: 'Full representation of the user\'s name.',
       maxLength: 120,
       example: 'Mary Sue van der McHenst'
+    },*/
+
+    firstName: {
+      type: 'string',
+      required: true,
+      description: 'The user\'s first name',
+      maxLength: 255,
+      example: 'John',
+    },
+
+    lastName: {
+      type: 'string',
+      required: true,
+      description: 'The user\'s last name',
+      maxLength: 255,
+      example: 'Doe',
     },
 
     isSuperAdmin: {
