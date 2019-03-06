653b58981a16a96d4f0a65b66da8b450b43eb3c8
Justin Parado <japarado@student.apc.edu.ph>
2019-03-06 18:04:14 +0800

    Corrected user session retrieval Updated res.send() to res.status().send()

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -8,13 +8,12 @@
 module.exports = {
   user: (req, res) =>
   {
-    console.log("WOW")
-    let userId = req.session.userId ? req.sessions.userId : false;
+    let userId = req.session.userId ? req.sessionsessions.userId : false;
     let context = {
       userId: userId
     };
     //return res.status(200).body(context);
-    res.send(200, context);
+    res.status(200).send(context);
   }
 };
 
