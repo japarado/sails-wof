06c04e96dc6f65d649c2edc7c4ef9ef0924a3594
pam <jparado_25@yahoo.com>
2019-03-08 14:29:57 +0800

    Formatting

--- config/routes.js
+++ config/routes.js
@@ -63,9 +63,9 @@ module.exports.routes = {
   'POST  /api/v1/deliver-contact-form-message':          { action: 'deliver-contact-form-message' },
 
   // START CUSTOM ROUTES
-  "GET /csrfToken": { action: "security/grant-csrf-token" },
-  "GET /api/v1/user": 'AuthController.user',
+  'GET /csrfToken': { action: 'security/grant-csrf-token' },
+  'GET /api/v1/user': 'AuthController.user',
 
   // Sample Testing Routes
-  "GET /userProfile/:id": 'SampleController.userProfile',
+  'GET /userProfile/:id': 'SampleController.userProfile',
 };
