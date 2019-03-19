dca859d655ab61b17e93ff13887c133cfbf784ac
japarado <jparado_25@yahoo.com>
2019-03-19 10:08:11 +0800

    Profile API
    Profile model definition and User - Profile model association

--- api/models/Profile.js
+++ api/models/Profile.js
@@ -12,8 +12,61 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    picture: {
+      type: 'string',
+      maxLength: 255,
+      required:false,
+      description: 'URI for the user\'s avatar',
+    },
 
+    companyName: {
+      type: 'string',
+      maxLength: 255,
+      required: false,
+      description: 'User\'s company',
+    },
 
+    contactNumber: {
+      type: 'string',
+      maxLength: 255,
+      required: false,
+      description: 'User\'s mobile number',
+    },
+
+    companySite: {
+      type: 'string',
+      maxLength: 255,
+      required: false,
+      description: 'URL of the user\'s current employment firm',
+    },
+
+    country: {
+      type: 'string',
+      maxLength: 255,
+      required: false,
+      description: 'User\'s country of residence',
+    },
+
+    addrOne: {
+      type: 'string',
+      maxLength: 255,
+      required: false,
+      description: 'Address, line one',
+    },
+
+    addrTwo: {
+      type: 'string',
+      maxLength: 255,
+      required: false,
+      description: 'Address, line two',
+    },
+
+    zipCode: {
+      type: 'number',
+      maxLength: 255,
+      required: false,
+      description: 'User zip/postal code',
+    },
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
