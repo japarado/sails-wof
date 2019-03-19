d65cf7baecec500c98cf054e7739a7dde003d26b
pam <jparado_25@yahoo.com>
2019-03-19 08:43:23 +0800

    Model relationship between service and work request

--- api/models/Service.js
+++ api/models/Service.js
@@ -1,5 +1,5 @@
 /**
- * WorkRequest.js
+ * Service.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -16,34 +16,7 @@ module.exports = {
       type: 'string',
       maxLength: 255,
       required: true,
-      description: 'The name of the requested project',
-    },
-
-    dueDate: {
-      type: 'ref',
-      columnType: 'datetime',
-      required: true,
-      description: 'Date when the project is due',
-    },
-
-    purpose: {
-      type: 'string',
-      maxLength: 255,
-      required: true,
-      description: 'What the work request endeavours to achieve',
-    },
-
-    audience: {
-      type: 'string',
-      maxLength: 255,
-      required: true,
-      description: 'Target market of the work request',
-    },
-
-    progress: {
-      type: 'number',
-      required: true,
-      description: 'Project completion expressed in a percent form',
+      description: 'Work request service text',
     },
 
 
@@ -55,14 +28,9 @@ module.exports = {
     //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-    descriptions: {
-      collection: 'description',
-      via: 'workRequests'
-    },
-
-    services: {
-      collection: 'service',
-      via: 'workRequests',
+    workRequests: {
+      collection: 'workRequest',
+      via: 'services',
     }
 
   },
