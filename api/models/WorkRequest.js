55e4836edb2a3f9a14dabf3d3ae7f23ad89f3ce0
pam <jparado_25@yahoo.com>
2019-03-19 08:23:41 +0800

    WorkRequest model definition

---
+++ api/models/WorkRequest.js
@@ -0,0 +1,62 @@
+/**
+ * WorkRequest.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: 'string',
+      maxLength: 255,
+      required: true,
+      description: 'The name of the requested project',
+    },
+
+    dueDate: {
+      type: 'ref',
+      columnType: 'datetime',
+      required: true,
+      description: 'Date when the project is due',
+    },
+
+    purpose: {
+      type: 'string',
+      maxLength: 255,
+      required: true,
+      description: 'What the work request endeavours to achieve',
+    },
+
+    audience: {
+      type: 'string',
+      maxLength: 255,
+      required: true,
+      description: 'Target market of the work request',
+    },
+
+    progress: {
+      type: 'number',
+      required: true,
+      description: 'Project completion expressed in a percent form',
+    }
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
